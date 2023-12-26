646cf11873cc358dcdea1d29eb78ebba593d5f4c
Saket Singh <150345873+singh-saket@users.noreply.github.com>
2023-12-26 18:27:13 +0530

    Update create-nondocker-desktop-config-file.ps1

--- create-nondocker-desktop-config-file.ps1
+++ create-nondocker-desktop-config-file.ps1
@@ -2,7 +2,7 @@
 $folderName = "config"
 
 #Declare the parent directory where you want to create the config folder
-$parentDirectory = "C:\Projects\Powershell"
+$parentDirectory = "C:\ProgramData\docker"
 
 #Combine the parent directory and folder name to get the full path
 $folderPath = Join-Path -Path $parentDirectory -ChildPath $folderName
@@ -13,60 +13,72 @@ $jsonFileName = "daemon.json"
 #Combine the Json file path with the above specified config folder path, as Json file should be the children of the config folder
 $jsonFilePath = Join-Path -Path $folderPath -ChildPath $jsonFileName
 
-#Check if config folder already exists
-if (-not (Test-Path $folderPath)) {
-    #Create the folder if it doesn't exist
-    New-Item -ItemType Directory -Path $folderPath | Out-Null
+if (Test-Path $parentDirectory -PathType Container) {
 	
 	Write-Host   # Empty Write-Host for a new line
-    Write-Host "Folder '$folderName' created successfully at $folderPath" -ForegroundColor Green
+	Write-Host "Docker engine is installed, checking for '$folderName'." -ForegroundColor Green
+
+	#Check if config folder already exists
+	if (-not (Test-Path $folderPath)) {
 	
-	if (-not (Test-Path $jsonFilePath)) {
-		#Define the JSON data
-		$jsonData = @{
-			"debug"        = $true
-			"dns"          = @("8.8.8.8")
-			"experimental" = $false
+		#Create the folder if it doesn't exist
+		New-Item -ItemType Directory -Path $folderPath | Out-Null
+	
+		Write-Host   # Empty Write-Host for a new line
+		Write-Host "Folder '$folderName' created successfully at $folderPath" -ForegroundColor Green
+	
+		if (-not (Test-Path $jsonFilePath)) {
+			#Define the JSON data
+			$jsonData = @{
+				"debug" = $true
+				"dns" = @("8.8.8.8")
+				"experimental" = $false
+			}
+		
+			#Create and add Json data to the file
+			$jsonData | ConvertTo-Json | Out-File -FilePath $jsonFilePath
+		
+			Write-Host   # Empty Write-Host for a new line
+			Write-Host "File '$jsonFileName' created successfully at $jsonFilePath" -ForegroundColor Green
+			Write-Host   # Empty Write-Host for a new line
 		}
-		
-		#Write the JSON data to the file
-		$jsonData | ConvertTo-Json | Out-File -FilePath $jsonFilePath
-		
-		Write-Host   # Empty Write-Host for a new line
-		Write-Host "File '$jsonFileName' created successfully at $jsonFilePath" -ForegroundColor Green
-		Write-Host   # Empty Write-Host for a new line
-	}
-	else
-	{
-		Write-Host   # Empty Write-Host for a new line
-		Write-Host "File '$jsonFileName' already exists at $jsonFilePath" -ForegroundColor Yellow
-		Write-Host   # Empty Write-Host for a new line
-	}
+		else
+		{
+			Write-Host   # Empty Write-Host for a new line
+			Write-Host "File '$jsonFileName' already exists at $jsonFilePath" -ForegroundColor Yellow
+			Write-Host   # Empty Write-Host for a new line
+		}
 	
-} else {
+	} else {
+		Write-Host   # Empty Write-Host for a new line
+		Write-Host "Folder '$folderName' already exists at $folderPath" -ForegroundColor Yellow
+	
+		if (-not (Test-Path $jsonFilePath)) {
+			# Define the JSON data
+			$jsonData = @{
+				"debug" = $true
+				"dns"  = @("8.8.8.8")
+				"experimental" = $false
+			}
+		
+			# Write the JSON data to the file
+			#$jsonData | Out-File -FilePath $jsonFilePathFinal
+			$jsonData | ConvertTo-Json | Out-File -FilePath $jsonFilePath
+		
+			Write-Host   # Empty Write-Host for a new line
+			Write-Host "File '$jsonFileName' created successfully at $jsonFilePath" -ForegroundColor Green
+			Write-Host   # Empty Write-Host for a new line
+		}
+		else
+		{
+			Write-Host   # Empty Write-Host for a new line
+			Write-Host "File '$jsonFileName' already exists at $jsonFilePath" -ForegroundColor Yellow
+			Write-Host   # Empty Write-Host for a new line
+		}
+	}
+}
+else {
 	Write-Host   # Empty Write-Host for a new line
-    Write-Host "Folder '$folderName' already exists at $folderPath" -ForegroundColor Yellow
-	
-	if (-not (Test-Path $jsonFilePath)) {
-		# Define the JSON data
-		$jsonData = @{
-			"debug"        = $true
-			"dns"          = @("8.8.8.8")
-			"experimental" = $false
-		}
-		
-		# Write the JSON data to the file
-		#$jsonData | Out-File -FilePath $jsonFilePathFinal
-		$jsonData | ConvertTo-Json | Out-File -FilePath $jsonFilePath
-		
-		Write-Host   # Empty Write-Host for a new line
-		Write-Host "File '$jsonFileName' created successfully at $jsonFilePath" -ForegroundColor Green
-		Write-Host   # Empty Write-Host for a new line
-	}
-	else
-	{
-		Write-Host   # Empty Write-Host for a new line
-		Write-Host "File '$jsonFileName' already exists at $jsonFilePath" -ForegroundColor Yellow
-		Write-Host   # Empty Write-Host for a new line
-	}
+	Write-Host "Docker engine is not installed, please install it first." -ForegroundColor Yellow
+	Write-Host   # Empty Write-Host for a new line
 }
